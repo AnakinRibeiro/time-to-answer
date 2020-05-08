1e2c530663128361170b7f4a31bfd475a189932b
AnakinRibeiro <eduardor.contato@outlook.com>
2020-05-08 18:21:59 -0300

    Refatorando views com Partials

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,6 +1,6 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
   before_action :verify_password, only: [:update]
-  before_action :set_admin, only: [:edit, :update]
+  before_action :set_admin, only: [:edit, :update, :destroy]
   
   def index
     @admins = Admin.all
@@ -30,11 +30,18 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
     end
   end
 
+  def destroy
+    if @admin.destroy
+      redirect_to admins_backoffice_admins_path, notice: "Administrador excluido com sucesso!"
+    else
+      render :index
+    end
+  end
+
   private 
   def params_admin
     params.require(:admin).permit(:email, :password, :password_confirmation)
   end
-
 
   def set_admin
     @admin = Admin.find(params[:id])
