80b15f68a6f0cc361d3a118e42bb282e20a5e527
AnakinRibeiro <eduardor.contato@outlook.com>
2020-05-12 18:11:18 -0300

    Implementando i18n para as models

--- app/helpers/admin_backoffice_helper.rb
+++ app/helpers/admin_backoffice_helper.rb
@@ -1,2 +1,9 @@
 module AdminBackofficeHelper
+    def translate_attribute(object = nil, method = nil)
+        if object && method
+            @questions.model.human_attribute_name(method)
+        else
+            "Informe os paramentros corretamente"
+        end
+    end
 end
