222e79462715dde74956e4720fa67190984973f2
AnakinRibeiro <eduardor.contato@outlook.com>
2020-05-14 18:10:23 -0300

    Corrigindo i18n, refatorando application_controller

--- app/helpers/admins_backoffice_helper.rb
+++ app/helpers/admins_backoffice_helper.rb
@@ -1,5 +1,5 @@
-module AdminBackofficeHelper
-    def translate_attribute(object = nil, method = nil)
+module AdminsBackofficeHelper
+  def translate_attribute(object = nil, method = nil)
     (object && method) ? object.model.human_attribute_name(method) : "Informe os parâmetros corretamente!"
   end
 end
