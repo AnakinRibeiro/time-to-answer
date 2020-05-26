846737ea6f9a25a617c6c4b8050f64ae281b55b7
AnakinRibeiro <eduardor.contato@outlook.com>
2020-05-26 11:19:53 -0300

    Janela modal / corrinndo data de nascimento

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   namespace :users_backoffice do
     get 'welcome/index'
   end
-  devise_for :users
   namespace :site do
     get 'welcome/index'
     get 'search', to: 'search#questions'
@@ -21,7 +20,10 @@ Rails.application.routes.draw do
     resources :questions # Perguntas
   end
   devise_for :admins, skip: [:registrations]
+  devise_for :users
+
   get 'inicio', to: 'site/welcome#index'
+  get 'backoffice', to: 'admins_backoffice/welcome#index'
 
   root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
